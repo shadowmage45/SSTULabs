21bdffafc02b84d35f93d532273d0db7844d4e53
shadowmage45 <shadowmage45@hotmail.com>
2017-08-31 17:32:47 -0600

    add additional project for build-tools; will be creating a .exe that will auto-increment the build number

---
+++ Plugin/SSTUTools/SSTUBuildTools/SSTUBuildTools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SSTUBuildTools
+{
+    class SSTUBuildTools
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
