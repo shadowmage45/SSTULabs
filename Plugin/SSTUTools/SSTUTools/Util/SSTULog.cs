2c61f489c083210db7635c12bdb4b6f842659a7e
shadowmage45 <shadowmage45@hotmail.com>
2018-04-30 18:04:41 -0600

    finally add a logging class...

---
+++ Plugin/SSTUTools/SSTUTools/Util/SSTULog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SSTUTools
+{
+    public static class SSTULog
+    {
+
+        public static readonly bool debugMode = true;
+
+        public static void stacktrace()
+        {
+            MonoBehaviour.print(System.Environment.StackTrace);
+        }
+
+        public static void log(string line)
+        {
+            MonoBehaviour.print("SSTU-LOG  : " + line);
+        }
+
+        public static void log(System.Object obj)
+        {
+            MonoBehaviour.print("SSTU-LOG  : " + obj);
+        }
+
+        public static void error(string line)
+        {
+            MonoBehaviour.print("SSTU-ERROR: " + line);
+        }
+
+        public static void debug(string line)
+        {
+            if (!debugMode) { return; }
+            MonoBehaviour.print("SSTU-DEBUG: " + line);
+        }
+
+    }
+}
